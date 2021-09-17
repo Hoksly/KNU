7a84227c263a3af1ea7e5a82a179ac76a4b5581f
hoksly <realhoksly@gmail.com>
2021-09-17 09:17:11 +0300

    stable overloaded classes

--- Lab_1/long_mantis_char.cpp
+++ Lab_1/long_mantis_char.cpp
@@ -11,9 +11,29 @@ class mnt
         int dgr = 0;
         int size = 256;
         bool is_negative = false;
+        bool is_zero = false;
    
+
+    
 };
 
+
+mnt copy(mnt a)
+{
+    mnt b;
+
+    char mas_cp[256];
+    b.dgr = a.dgr;
+    b.is_negative = a.is_negative;
+    b.size = a.size;
+    b.is_zero = a.is_zero;
+
+    for(int i = 0; i < a.size; i++)
+        b.mas[i]= a.mas[i];
+
+    return b;
+    
+}
 
 void reverse(mnt &a)
 {
@@ -37,6 +57,11 @@ mnt convert(string s, int n)
         sz = s.size()-1;
         is_negative = true;
     }
+    else if(s[0] == '0' && s.size() == 1)
+    {
+        a.is_zero = true;
+        return a;
+    }
     else
     {
         sz = s.size();
@@ -50,7 +75,7 @@ mnt convert(string s, int n)
     for(int i = 1; i < s.size(); i++)
     {
         a.mas[i] = s[i] - '0';
-        //cout << (int) ret[i] << ' ';
+        //cout << (int) a.mas[i] << ' ';
     }    
 
     
@@ -76,15 +101,19 @@ void print(mnt a)
         cout << '0';
     }
 
-    for(int i = 0; i <= a.dgr; i++)
+    for(int i = 0; i <= min(a.dgr, a.size-1); i++)
         cout << (int) a.mas[i];
     
-    if(a.dgr != a.size -1 && a.dgr >= 0)cout << '.' ;
+    if(a.dgr != a.size -1 && a.dgr >= 0 && a.size > a.dgr)cout << '.' ;
 
     for(int i = max(a.dgr+1, 0); i < a.size; i++)
         cout << (int) a.mas[i];
+    
+    for(int i = 0; i < a.dgr - a.size + 1; i++)
+        cout << '0';
 
-    cout << endl << a.dgr << endl;
+    //cout << endl << a.dgr << endl;
+    cout << endl;
 }
 
 void pr_mas(char *a, int s)
@@ -102,6 +131,8 @@ mnt take_input()
     getline(cin, s);
     cin >> d;
     getline(cin, k);
+    //cout << s.size() << ' ' << d << endl;
+    //cout << s << endl;
     return(convert(s, d));
 }
 
@@ -113,7 +144,12 @@ mnt sbm2(mnt a, mnt b)
     001.234
     */
     mnt ret;
+    if(a.is_zero)
+        return b;
+    if(b.is_zero)
+        return a;
 
+    //cout << "gg123" << endl;
     int a_dgr = a.dgr, b_dgr = b.dgr, shift = 0;
     bool null_writed;
     while(a_dgr < 0 || b_dgr < 0)
@@ -238,10 +274,12 @@ mnt sbm2(mnt a, mnt b)
         }
     }
     int n_dgr = max(a.dgr, b.dgr);
+    
     if(add_one)
     {
         ret.mas[shift] = 1;
         n_dgr++;
+        shift++;
     }
 
     
@@ -250,6 +288,7 @@ mnt sbm2(mnt a, mnt b)
     ret.size = shift;
     ret.dgr = n_dgr;
     ret.is_negative = false;
+    ret.is_zero = false;
 
     reverse(ret);
     return ret;
@@ -282,6 +321,14 @@ bool bigger (mnt a, mnt b)
 mnt substraction(mnt a, mnt b, bool is_negative = false)
 {
     mnt ret;
+    if(a.is_zero)
+    {
+        ret = copy(b);
+        ret.is_negative = !b.is_negative;
+    }
+    if(b.is_zero)
+        return copy(a);
+
     int remove_one = 0, val;
     bool writed_number;
     // a - b, 
@@ -397,6 +444,190 @@ mnt substraction(mnt a, mnt b, bool is_negative = false)
 
 }
 
+mnt multipicata_one_digit(mnt &a, char d, char add_dgr)
+{
+    mnt b;
+    if (d == 0)
+    {
+        b.dgr = 0;
+        b.is_negative = false;
+        b.size = 1;
+        b.mas[0] = (char) 0;
+        b.is_zero = true;
+        return b;
+    }
+    b = copy(a);
+   
+
+    for(char i = 1; i < d; i++)
+    {
+        b = sbm2(b, a);
+    }
+    b.dgr += (int) add_dgr;
+
+    return b;
+}
+
+mnt multipication(mnt &a, mnt &b)
+{
+    mnt res, q;
+    res.dgr = 0;
+    res.is_negative = false;
+    res.size = 1;
+    res.is_zero = true;
+    res.mas[0] = (char) 0;
+    //print(b);
+    for(int i = 0; i < a.size; i++)
+    {
+        
+        q = multipicata_one_digit(b, a.mas[a.size - i - 1], a.dgr - a.size  + i+1);
+        
+        
+        res = sbm2(res, q);
+    }
+
+    return res;
+}
+
+
+class LongFloat
+{
+   
+public:
+    mnt value;
+    
+    LongFloat (mnt a)
+    {
+        value = a;
+    }
+
+    LongFloat ()
+    {
+
+    }
+
+    int size()
+    {
+        return value.size;
+    }
+    int dgr()
+    {
+        return value.dgr;
+    }
+
+    char* mas()
+    {
+        return value.mas;
+    }
+
+    bool is_zero()
+    {
+        return value.is_zero;
+    }
+
+    bool is_negative()
+    {
+        return value.is_negative;
+    }
+
+    LongFloat operator + (LongFloat b)
+    {
+    
+        if(value.is_negative && b.is_negative())
+        {
+            mnt ret = sbm2(value, b.value);
+            ret.is_negative = true;
+            return LongFloat(ret);
+        }
+            
+        if(value.is_negative)
+        {
+            return LongFloat(substraction(b.value, value));
+        }
+        if(b.is_negative())
+        {
+            return LongFloat(substraction(value, b.value));
+        }
+        else
+            return LongFloat(sbm2(value, b.value));
+    }
+
+    LongFloat operator- (LongFloat b)
+    {
+        if(value.is_negative && b.is_zero())
+        {
+            mnt ret = substraction(b.value, value);
+            //ret.is_negative = true;
+            return ret;
+        }
+            
+        else if(value.is_negative)
+        {
+            mnt ret = sbm2(value, b.value);
+            ret.is_negative = true;
+            return substraction(b.value, value);
+        }
+        else if(b.is_negative())
+        {
+            return sbm2(value, b.value);
+        }
+        else
+            return substraction(value, b.value);    
+    }
+
+    LongFloat& operator = (LongFloat a)
+    {
+        value = copy(a.value);
+
+        return *this;
+    }
+
+    LongFloat operator* (LongFloat b)
+    {
+        if(value.is_negative && b.is_negative())
+            return LongFloat(multipication(value, b.value));
+        if(value.is_negative || b.is_negative())
+        {
+            mnt ret = multipication(value, b.value);
+            ret.is_negative = true;
+            return LongFloat(ret);
+        }
+        return LongFloat(multipication(value, b.value));
+
+    }
+
+    void print()
+    {
+        cout << endl;
+    if(value.is_negative)
+        cout << '-';
+
+    if(value.dgr < 0)
+        cout << "0.";
+
+    for(int i = value.dgr; i < -1; i++)
+    {
+        cout << '0';
+    }
+
+    for(int i = 0; i <= min(value.dgr, value.size-1); i++)
+        cout << (int) value.mas[i];
+    
+    if(value.dgr != value.size -1 && value.dgr >= 0 && value.size > value.dgr)cout << '.' ;
+
+    for(int i = max(value.dgr+1, 0); i < value.size; i++)
+        cout << (int) value.mas[i];
+    
+    for(int i = 0; i < value.dgr - value.size + 1; i++)
+        cout << '0';
+
+    //cout << endl << a.dgr << endl;
+    cout << endl;
+    }
+    
+
+};
+
 
 int main()
 {
@@ -404,13 +635,20 @@ int main()
     a = take_input();
     b = take_input();
     
-
+    
     //cout << a.dgr << ' ' << b.dgr << endl;
- 
-   mnt res = substraction(a, b);
-
+    LongFloat A(a);
+    LongFloat B(b);
+    LongFloat C;
+   //mnt res = multipicata_one_digit(a, 2, 0);
+   //mnt res = copy(a);
+   // mnt res = sbm2(a, b);
     //pr_mas(res.mas, res.size);
-   print(res);
+    
+    C = A * B;
+    C.print();
+    
+    
    //cout << (int) res.mas[0] << endl;
    
 

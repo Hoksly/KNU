68f33669022c1d8a0bc2946007a23bb3fc627bb3
hoksly <realhoksly@gmail.com>
2021-10-24 12:49:54 +0300

    Stable with INSERT

--- Lab_5/main.cpp
+++ Lab_5/main.cpp
@@ -1,4 +1,3 @@
-//#include <iostream>
 #include <stdio.h>
 #include <iostream>
 #include <vector>
@@ -10,19 +9,29 @@ using namespace std;
 char FILENAME[] = "data.dat";
 const int MAX_NAME_LEN = 40;
 const int MAX_DATE_OF_BIRTH_LEN = 11;
-const int MAX_LINE_LEN = 150;
+const int MAX_LINE_LEN = 350;
 const int MAX_COMMAND_LEN  = 15;
+
 char CLOSE_CONDITION[] = ":q";
+char CLOSE_CONDITION_UPPER[] = ":Q";
 
 const int MAX_VALUE_LEN = 40;
 const int MAX_PARAM_LEN = 20;
-
+const int MAX_BOOL_LEN = 2;
 //commands
 char INSERT_COMMAND[] = "INSERT";
 char DELETE_COMMAND[] = "DELETE";
 char UPDATE_COMMAND[] = "UPDATE";
 char SELECT_COMMAND[] = "SELECT";
 char WRITE_DOWN_COMMAND[] = "WRITE_DOWN";
+
+//other variables
+char EMPTY_STRING[] = "";
+char NAME_PARAM[] = "name";
+char SECOND_NAME_PARAM[] = "second_name";
+char IS_MALE_PARAM[] = "is_male";
+char TELEGRAM_ID_PARAM[] = "telegram_id";
+char DATE_OF_BIRTH_PARAM[] = "date_of_birth";
 
 /*
 struct Person
@@ -35,6 +44,23 @@ struct Person
 
 };
 */
+
+struct str_with_pos
+{
+    string s = "";
+    int pos = 0;
+
+    char get_ch()
+    {
+        
+        return s[pos++];
+    }
+    char get_ch_nm()
+    {
+        return s[pos];
+    }
+
+};
 struct Person
 {   
     char name[40];
@@ -44,6 +70,17 @@ struct Person
     char date_of_birth[11];
 
 };
+
+
+void print_map(map<char*, char*> A)
+{
+    cout << "Printing map" << endl;
+    auto i = A.begin();
+    for(;i != A.end(); i++)
+    {
+        cout << i->first << ' ' << i->second << endl;
+    }
+}
 
 int read_file(char *filename, vector<Person*> *people)
 {
@@ -84,9 +121,12 @@ int read_file(char *filename, vector<Person*> *people)
         }
         else
         {
+            //cout << (long) ptr << endl;
+            
             people->push_back((Person*) c2);
             ctr = 0;
             Person *ptr = (Person *) malloc(sizeof(Person));
+            //cout << (long) ptr << endl;
             c = (char *) ptr;
             c2 = c;
 
@@ -154,6 +194,7 @@ int write_in_file(char *filename, vector<Person*> *people)
     }
     return 0;
 }
+
 int add_new_person(vector<Person*> *people, char *name, char *second_name, char *date_of_birthday, long telegram_id, bool is_male)
 {
     Person *ptr = new Person;
@@ -167,71 +208,209 @@ int add_new_person(vector<Person*> *people, char *name, char *second_name, char 
     ptr->is_male = is_male;
     ptr->telegram_id = telegram_id;
     people->push_back(ptr);
+
+    cout << ptr->name << endl;
+    cout << ptr->second_name << endl;
+    cout << ptr->telegram_id << endl;
+    cout << ptr->date_of_birth << endl;
+
+    return 0;
 }
 
- void clear_spaces(char **ch) 
+ void clear_spaces(str_with_pos &ch, int &i) 
 {
-    while (**ch == ' ')
-        (*ch)++;
+    while (ch.get_ch_nm() == ' ' && ch.get_ch_nm() != '\0')
+        {ch.pos++;
+        
+        if(i++ > MAX_LINE_LEN)
+            {cout <<"max line len: " << MAX_LINE_LEN << endl;}
+        
+        }
 
 }
-int find_word(char **ch, char *word)
+/*
+int find_word(char **ch, char *word, int &i)
 {
     while(('a' <= **ch && **ch <= 'z') || ('A' <= **ch && **ch <= 'Z'))
     {
         *word = **ch;
         word++;
         (*ch)++;
+        if(i++ > MAX_LINE_LEN)
+            {cout <<"max line len: " << MAX_LINE_LEN << endl; return 1;}
     }
+    
 
     return 0;
 }
+*/
 
-int find_command(char **ch, char *command)
+
+int find_command(str_with_pos &ch, char *command, int &i)
 {
-    int i = 0;
-    while(i < MAX_COMMAND_LEN && **ch != ' ')
+    int q = 0;
+    while(q < MAX_COMMAND_LEN && ch.get_ch_nm() != ' ')
     {
-        *command = **ch;
-        (*ch)++;
+        *command = toupper(ch.get_ch());
+        
         command++;
+        if(i++ > MAX_LINE_LEN)
+            {cout <<"max line len: " << MAX_LINE_LEN << endl; return 1;}
     }
-    if(**ch == 0 || i >= 15)
+    if(ch.get_ch_nm() == 0 || q >= 15)
         {cout << "Error while parsing command"; return 1;}
     *command = '\0';
     return 0;
 }
-int find_param_and_value(char **ch, char *param, char *value)
-{
 
+int find_param_and_value(str_with_pos &ch, string &param, string &value, int &i)
+{
+    char s;
+    clear_spaces(ch, i);
+    while(!isblank(ch.get_ch_nm()))
+    {
+        
+        param += ch.get_ch();
+        
+        
+        if(i > MAX_LINE_LEN)
+            {cout <<"max line len: " << MAX_LINE_LEN << endl; return 1;}
+    }
+    clear_spaces(ch, i);
+    if(ch.get_ch_nm() == '=')
+    {   
+        //cout << "HERE2" << endl;
+        ch.pos++;
+        clear_spaces(ch, i);
+        s = ch.get_ch_nm();
+        while(!isblank(ch.get_ch_nm()) && ch.get_ch_nm() != ',' && ch.get_ch_nm() != '\0')
+        {
+            value += ch.get_ch();
+            
+            
+            
+            if(i++ > MAX_LINE_LEN)
+            {cout <<"max line len: " << MAX_LINE_LEN << endl; return 1;}
+        }
+    }
+    //cout << "OUT" << endl;
     return 0;
 }
-int find_params(char **ch, map<char*, char*> *list_of_params)
+void print_10(char **ch)
 {
-    char *param = new char[MAX_PARAM_LEN];
-    char *value = new char[MAX_VALUE_LEN];
-    while(true)
-    {
-        find_param_and_value(ch, param, value);
-        list_of_params->insert(param, value);
-        char *param = new char[MAX_PARAM_LEN];
-        char *value = new char[MAX_VALUE_LEN];
-        
-        clear_spaces(ch);
-        if(**ch != ',')
-            return 0;
-
-    }
+    for(int i = 0; i < 30; i++)
+        cout << (*ch)[i];
+    cout << endl;
+}
+// INSERT name = Andrew, second_name = Nelitskiy, is_male = 1, telegram_id = 123, date_of_birth = 30:09:2004
+string covert_char_to_string(char *param)
+{
+    string s;
+    for(; *param; param++)
+        s += *param;
+    //cout << s.length() << endl;
+    return s;
 }
 
-int parse_command(char *input, vector<Person*> *people)
+int find_params_and_values(str_with_pos &ch, vector<pair<string, string>> &list_of_params, int &i)
 {
-    char **ch = &input;
-    char *main_command = new char[MAX_COMMAND_LEN];
-    find_command(ch, main_command);
-    clear_spaces(ch);
-    map <char*, char*> params_list;
+    
+    while(true)
+    {   //cout <<"HERE" << endl;
+        string param = "", value = "";
+        find_param_and_value(ch, param, value, i);
+        pair<string, string> A =  make_pair(param, value);
+        cout << A.first << ' ' << A.second << endl;
+        
+        list_of_params.push_back(A);
+        //cout << list_of_params->find(covert_char_to_string(param))->first << ' ' << list_of_params->find(covert_char_to_string(param))->second << endl;  
+        
+        
+        clear_spaces(ch, i);
+       
+        
+        if(ch.get_ch_nm() != ',' || ch.get_ch_nm() == '\0')
+            return 0;
 
+        ch.pos++; // symbol ','
+        clear_spaces(ch, i);
+        if(i++ > MAX_LINE_LEN)
+            {cout <<"max line len: " << MAX_LINE_LEN << endl; return 1;}
+
+    }
+    
+   
+}
+
+int command_insert(str_with_pos &ch, vector<Person*> *people, int &i)
+{
+    vector <pair<string, string>> params_list;
+    find_params_and_values(ch, params_list, i);
+    
+    char *is_male = new char[MAX_BOOL_LEN];
+    char *date_of_birth = new char[MAX_DATE_OF_BIRTH_LEN];
+    //print_map(params_list);
+    /*
+    if(!strcmp(EMPTY_STRING, params_list.find(NAME_PARAM)->second) && !strcmp(EMPTY_STRING, params_list.find(SECOND_NAME_PARAM)->second))
+    {
+        long telegram_id;
+        bool is_realy_male;
+        date_of_birth = params_list.find(DATE_OF_BIRTH_PARAM)->second;
+        is_male = params_list.find(IS_MALE_PARAM)->second;
+        
+        if((int) params_list.find(TELEGRAM_ID_PARAM)->second[0] > 0)
+            telegram_id = strtol(params_list.find(TELEGRAM_ID_PARAM)->second, 0, 10);
+        else
+            telegram_id = 0;
+        
+        if(date_of_birth[0] < 0)
+            date_of_birth = EMPTY_STRING;
+        
+        if(is_male[0] < 0)
+            is_realy_male = true;
+        else
+            is_realy_male = (bool) strtol(is_male, 0, 10);
+        cout << "HERE" << endl;
+        cout << params_list.find(NAME_PARAM)->second << endl;
+    
+        add_new_person(people,params_list.find(NAME_PARAM)->second, 
+        params_list.find(SECOND_NAME_PARAM)->second, 
+        date_of_birth, telegram_id, is_realy_male);
+
+
+    }
+    else
+    {
+        cout << "Error in insert command: gap 'name' and 'second_name' cannot be empty or been ignored" << endl;
+        return 1;
+    }
+    */
+    /*
+    
+    strcpy(name, params_list.find(NAME_PARAM)->second);
+    strcpy(second_name, params_list.find(SECOND_NAME_PARAM)->second);
+    strcpy(telegram_id, params_list.find(TELEGRAM_ID_PARAM)->second);
+    strcpy(is_male, params_list.find(IS_MALE_PARAM)->second);
+    strcpy(date_of_birth, params_list.find(DATE_OF_BIRTH_PARAM)->second);
+    */
+
+    
+    
+
+   return 0;
+}
+
+
+
+int parse_command(str_with_pos &input, vector<Person*> *people)
+{
+
+    char *main_command = new char[MAX_COMMAND_LEN];
+    int i = 0;
+    find_command(input, main_command, i);
+    clear_spaces(input, i);
+    
+    cout <<"Comand: "<< main_command << endl;
 
     if(!strcmp(main_command, WRITE_DOWN_COMMAND))
     {
@@ -240,7 +419,8 @@ int parse_command(char *input, vector<Person*> *people)
     
     else if(!strcmp(main_command, INSERT_COMMAND))
     {
-
+        
+        command_insert(input, people, i);
     }
     
     else if(!strcmp(main_command, UPDATE_COMMAND))
@@ -275,41 +455,46 @@ int process()
     if(read_file(FILENAME, &people))
         return 1;
 
-    /*
+   
     //simple_write();
     //Person pp = *(people[0]);
+    //Debug
+   
     cout << people.size() << endl;
     for(int i = 0; i < people.size(); i++)
     {
         Person pp = *(people[i]);
         cout << pp.name << ' ' << pp.second_name << ' ' << pp.date_of_birth << endl;
     }
-    */
+    
     cout << endl <<endl;
     cout << "Welcome to the consol version of TelegramDb controller"<< endl;
     cout << "Some tips for it's usage: " << endl;
     cout <<"To select something: SELECT param1, param2... WHERE statement1 = s-th1, statement2 = s-th2" << endl;
     cout <<"To delete something: DELETE WHERE cond1, cond2..." << endl;
     cout << "To insert something: INSERT param1 = value1, param2 = value2..." << endl;
-    cout << "To update something UPDATE param1 = value1, param2 = value2... WHERE condition1, condition2... ";
+    cout << "To update something: UPDATE param1 = value1, param2 = value2... WHERE condition1, condition2... ";
     cout << "To write changes: WRITE_DOWN (automaticly write when close)" << endl;
-    cout << "To exit program "<<CLOSE_CONDITION << endl;
+    cout << "To exit program: '"<<CLOSE_CONDITION <<'\''<< endl;
     cout << "Additional information about all functions: <Command> --help" << endl << endl;
-    char *input = new char[MAX_LINE_LEN];
+    
+    str_with_pos input;
     while (true)
     {
         
-        cin.getline(input, MAX_LINE_LEN);
-        if(!strcmp(input, CLOSE_CONDITION))
+        getline(cin, input.s);
+        
+        if(input.s == CLOSE_CONDITION || input.s == CLOSE_CONDITION_UPPER)
         {
-            write_in_file(FILENAME, &people);
+            //write_in_file(FILENAME, &people);
             cout << "Exiting..." << endl;
             break;
         }
+        parse_command(input, ptr);
 
 
     }
-
+   
 
     return 0;
 }
@@ -322,3 +507,10 @@ int main()
     return 0;   
 
 }
+
+/*
+Adding new person with command insert not wotking properly, debug with cin
+
+INSERT name = Andrew, second_name = Nelitskiy, is_male = 1, telegram_id = 123, date_of_birth = 30:09:2004
+
+*/

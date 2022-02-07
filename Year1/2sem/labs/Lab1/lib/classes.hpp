bf56e7be37a59504e2719b1972a514b6dfd54c0a
hoksly <realhoksly@gmail.com>
2022-02-07 14:46:38 +0200

    Unstable with List class

--- Year1/2sem/labs/Lab1/lib/classes.hpp
+++ Year1/2sem/labs/Lab1/lib/classes.hpp
@@ -1,5 +1,6 @@
 #pragma once
 #include <string>
+#include <string.h>
 
 struct command
 {
@@ -9,4 +10,41 @@ struct command
     int n;
 
     command(std::string, std::string, int , bool); 
+    
+};
+
+typedef struct MarkovPart
+{
+    char val;
+    MarkovPart *next = nullptr;
+} *MarkovPtr;
+
+class MarkovList
+{
+    int size = 0;
+    MarkovPtr FIRST = nullptr;
+
+    //functions
+    MarkovPtr GiveLast();
+    MarkovPtr Search(MarkovPtr, std::string);
+
+    bool IsIt(MarkovPtr, std::string, int);
+
+    MarkovPtr MoveTo(MarkovPtr, int);
+
+    //operations 
+    void CreateNewParts(int);
+    void DeleteParts(MarkovPtr, int);
+    void ReplaceValue(MarkovPtr, std::string, int);
+    void AddParts(MarkovPtr, int);
+    void AddAndSet(MarkovPtr, std::string);
+    void Replace(MarkovPtr, std::string, std::string);
+
+    public:
+    MarkovList(char *);
+    MarkovList(std::string);
+
+    int size();
+    int replace(std::string, std::string);
+
 };

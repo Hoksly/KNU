d3017ef1cbb52c2addfff2d57ba48e3cf523ae7c
hoksly <realhoksly@gmail.com>
2022-09-28 19:28:36 +0300

    More methods implemented, doctest fails to compile

--- Year2/1sem/OOP/Labs/Lab1/main.cpp
+++ Year2/1sem/OOP/Labs/Lab1/main.cpp
@@ -1,4 +1,17 @@
+#include "vlib/victor.hpp"
+#include <iostream>
+#include "doctest/doctest/doctest.h"
+
+using namespace std;
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+
+TEST_CASE("Please work")
+{
+    int a = 5;
+
+    CHECK(a == 5);
+}
+
 int main()
 {
-    return 0;
 }
--- Year2/1sem/OOP/Labs/Lab1/vlib/victor.hpp
+++ Year2/1sem/OOP/Labs/Lab1/vlib/victor.hpp
@@ -5,7 +5,7 @@
 #ifndef VICTOR_H
 #define VICTOR_H
 
-typedef int iterator; // maybe i will write it later
+typedef int _iterator; // maybe i will write it later
 typedef long long v_size;
 
 template <class T>
@@ -17,7 +17,7 @@ private:
 
     T *_base = nullptr;
 
-    T __get_element(const iterator index);
+    T __get_element(const _iterator index);
     void __copy_victor(T *new_place);
     void __move_left(v_size from, v_size n_of_positions);
     void __move_right(v_size from, v_size n_of_positions);
@@ -27,29 +27,230 @@ public:
     victor(v_size size);
     victor(v_size size, T init_item);
 
-    inline bool size() { return _size; }
+    inline v_size size() { return _size; }
+    inline v_size capacity() { return _capacity; }
 
     victor &clear();
-    victor &erase(const iterator position);
-    victor &erase(const iterator first, const iterator last); // removes elements in range [first, last]
+    victor &erase(const _iterator position);
+    victor &erase(const _iterator first, const _iterator last); // removes elements in range [first, last]
 
-    iterator insert(const iterator &position, const T &element);
+    _iterator insert(const _iterator &position, const T &element);
     victor &push_back(const T &);
     T pop_back();
 
-    victor &resize(const iterator target_size);
-    victor &reserve(const iterator additional_space);
+    victor &resize(const _iterator target_size);
+    victor &reserve(const v_size additional_space);
     victor &shrink_to_fit();
 
     victor copy();
 
-    void swap(const iterator first, const iterator second);
+    void swap(const _iterator first, const _iterator second);
     v_size search(const T &value);
 
     T &operator[](v_size index);
+    victor operator=(const victor &other_victor);
 
     // debuging
     void print();
+    std::string str();
 };
+
+template <class T>
+void victor<T>::__copy_victor(T *new_place)
+{
+    T *element_ptr = _base;
+
+    for (v_size i = 0; i < _size; ++i, element_ptr++, new_place++)
+    {
+        *new_place = *element_ptr;
+    }
+}
+
+template <typename T>
+void victor<T>::__reallocate_victor(v_size additional_size)
+{
+    T *new_victor = new T[_capacity + additional_size];
+    this->__copy_victor(new_victor);
+    delete _base;
+
+    _base = new_victor;
+
+    _capacity += additional_size;
+}
+
+template <class T>
+void victor<T>::__move_left(v_size position, v_size n_of_positions)
+{
+    // ACHTUNG
+    // This will overwrite element with positions [position-n_of_positions, position-1]
+
+    v_size tmp_var;
+    for (v_size i = 0; i < n_of_positions; ++i)
+    {
+        for (v_size cur_position = position; cur_position < _size - i; cur_position++)
+        {
+            _base[cur_position - 1] = _base[cur_position];
+        }
+    }
+}
+template <class T>
+void victor<T>::__move_right(v_size from, v_size n_of_positions)
+{
+    std::cout << "Alright" << std::endl;
+    for (v_size i = 0; i < n_of_positions; ++i)
+    {
+        for (v_size cur_position = _size + i; cur_position > from + i; --cur_position)
+        {
+            std::cout << cur_position << std::endl;
+            _base[cur_position] = _base[cur_position - 1];
+        }
+    }
+}
+
+template <class T>
+victor<T>::victor(v_size size)
+{
+    if (size < 0)
+    {
+        throw std::bad_alloc(); // "size can not be less than zero"
+    }
+
+    _base = new T[size];
+
+    _size = 0;
+    _capacity = size;
+}
+
+template <class T>
+victor<T>::victor(v_size size, T init_object) : victor(size)
+{
+
+    T *tmp = _base;
+    for (long long i = 0; i < size; ++i, tmp++)
+    {
+        *tmp = init_object;
+    }
+    _size = size;
+}
+
+template <class T>
+victor<T> &victor<T>::clear()
+{
+    _size = 0;
+    return *this;
+}
+
+template <class T>
+victor<T> &victor<T>::erase(const _iterator position)
+{
+    if (position >= _size || position < 0)
+        throw std::__throw_bad_function_call(); // "Postion is out of victor size"
+
+    __move_left(position + 1, 1);
+
+    _size--;
+
+    return *this;
+}
+
+template <class T>
+_iterator victor<T>::insert(const _iterator &position, const T &element)
+{
+    if (position > _size || position < 0)
+        throw std::__throw_bad_function_call; // "Postion is out of victor size"
+    if (position == _size)
+    {
+        this->push_back(element);
+        return position;
+    }
+    // in case we don't have enough place
+    if (_size == _capacity)
+        this->__reallocate_victor(_capacity + 1);
+
+    __move_right(position, 1);
+    _base[position] = element;
+
+    _size++;
+
+    return position;
+}
+
+template <class T>
+victor<T> &victor<T>::push_back(const T &element)
+{
+
+    if (_size == _capacity)
+        this->__reallocate_victor(_capacity + 1); // not a greatest formula, but let it be...
+    std::cout << "HERE" << element << ' ' << _size << std::endl;
+    _base[_size] = element; // index of last element is size-1
+
+    _size++;
+    return *this;
+}
+
+template <class T>
+T victor<T>::pop_back()
+{
+    if (_size > 0)
+    {
+        T poped_element = _base[size];
+        _size--;
+    }
+    else
+    {
+        throw std::out_of_range(); //"Can't pop empty victor"
+    }
+}
+
+template <class T>
+victor<T> &victor<T>::reserve(const v_size additional_space)
+{
+    this->__reallocate_victor(additional_space);
+    return *this;
+}
+
+template <class T>
+victor<T> &victor<T>::shrink_to_fit()
+{
+    if (_size != _capacity)
+    {
+        T new_victor = new T[_size];
+        this->__copy_victor(new_victor);
+        delete _base;
+        _base = new_victor;
+
+        _capacity = _size;
+    }
+}
+
+template <class T>
+T &victor<T>::operator[](v_size index)
+{
+    if (index >= 0 && index < _size)
+        return _base[index];
+
+    throw std::out_of_range("Index is out of victor range"); // "index out of victor range"
+}
+
+template <class T>
+void victor<T>::print()
+{
+    for (v_size i = 0; i < _size; ++i)
+        std::cout << _base[i] << ' ';
+
+    std::cout << std::endl;
+}
+
+template <class T>
+std::string victor<T>::str()
+{
+    std::string s;
+    // in case it can't convert objects to string it will throw an error ?
+
+    for (v_size i = 0; i < _size; ++i)
+        s += to_string(_base[i]) + ' ';
+
+    return s;
+}
 
 #endif

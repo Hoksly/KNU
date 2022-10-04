7841145918ede7869e0973c95219475e3cd46e7b
hoksly <realhoksly@gmail.com>
2022-10-04 20:49:29 +0300

    Tests added, Basic class addes

---
+++ Year2/1sem/OOP/Labs/Lab1/lib/Figure.hpp
@@ -0,0 +1,9 @@
+// just an abstract class
+
+#ifndef _FIGURE_HPP
+#define _FIGURE_HPP
+class Figure
+{
+};
+
+#endif
---
+++ Year2/1sem/OOP/Labs/Lab1/lib/Symetry_test.cpp
@@ -0,0 +1,23 @@
+#include "/home/hoksly/Github/KNU/Year2/1sem/OOP/Labs/Lab1/doctest/doctest/doctest.h"
+#include "Symetry.hpp"
+
+TEST_CASE("Point-Line symetry")
+{
+    Symetry s;
+
+    Point A(0, 0);
+    Point B(0, 5);
+
+    Line l(Point(1, 1), Point(5, 5));
+    CHECK(l.b() == 0);
+    CHECK(l.k() == 1);
+
+    Point A_sym = s.symetry(A, l);
+    Point B_sym = s.symetry(B, l);
+
+    CHECK_EQ(A.x(), A_sym.x()); // A lies on a line l;
+    CHECK_EQ(A.y(), A_sym.y());
+
+    CHECK_EQ(B_sym.x(), 5);
+    CHECK_EQ(B_sym.y(), 0);
+}

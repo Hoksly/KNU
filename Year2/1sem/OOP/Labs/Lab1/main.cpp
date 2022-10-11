ab159d9fe82b19fb387e2316f4c604724ec7169e
hoksly <realhoksly@gmail.com>
2022-10-11 12:26:17 +0300

    main commit

--- Year2/1sem/OOP/Labs/Lab1/main.cpp
+++ Year2/1sem/OOP/Labs/Lab1/main.cpp
@@ -1,19 +1,7 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include "vlib/victor.hpp"
 #include <iostream>
-#include "vlib/sort.hpp"
-using namespace std;
-#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
-
-int main()
-{
-
-    victor<int> v(10);
-    for (int i = 0; i < 10; ++i)
-    {
-        v.push_back(10 - i);
-    }
-    cout << v << endl;
-    v.sort();
-
-    cout << v << endl;
-}
+#include "vlib/universalsort.hpp"
+#include "vlib/spacialsort.hpp"
+#include <vector>
+#include "doctest/doctest/doctest.h"

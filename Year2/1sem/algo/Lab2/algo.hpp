cf42592315cc03ae70798add62000da52ab0a834
hoksly <realhoksly@gmail.com>
2022-12-25 15:13:13 +0200

    RabinCarp and Horspool algorithms implemented

--- Year2/1sem/algo/Lab2/algo.hpp
+++ Year2/1sem/algo/Lab2/algo.hpp
@@ -1,4 +1,16 @@
+#pragma once
+#include <string>
+#define ll long long
+#include <vector>
+#include <unordered_map>
 
+#ifndef SEARCH_ALGORITHMS_HPP
+#define SEARCH_ALGORITHMS_HPP
 long long naiveSearch(std::string text, std::string part);
+
 long long horspoolSearch(std::string text, std::string part);
-long long rabinCarpSearch(std::string text, std::string part);
+
+long long rabinCarpSearch(std::string text, std::string part,
+                          int charsInAplhabet = 256, ll base = 717,
+                          ll primeOne = 87178291199, bool checkCollisions = false);
+#endif

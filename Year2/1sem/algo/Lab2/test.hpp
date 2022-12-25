4dcb6de79bea6e4888ee9f42c301eb0855b71fd2
hoksly <realhoksly@gmail.com>
2022-12-25 15:14:26 +0200

    universal tests added

--- Year2/1sem/algo/Lab2/test.hpp
+++ Year2/1sem/algo/Lab2/test.hpp
@@ -1,5 +1,24 @@
+#pragma once
 #include "algo.hpp"
 #include "string"
+#include <vector>
+#include <chrono>
 
-void testRandom(long long textBeg, long long textEnd, long long textStep,
-                long long partBeg, long long partEnd, long long partStep);
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+#define ll long long
+
+#ifndef TEST_ALGORITHMS_HPP
+#define TEST_ALGORITHMS_HPP
+
+void testRandom(ll textBeg, ll textEnd, ll textStep,
+                ll partBeg, ll partEnd, ll partStep);
+void testReal(std::string filename, std::string functionName,
+              ll *search(const std::string, const std::string),
+              ll partBeg = 10, ll partEnd = 1011, ll partStep = 1000);
+
+const std::string CHARS_FOR_TEST = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+#endif
